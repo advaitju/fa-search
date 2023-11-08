a976a04a0736d4c86eaef7a7fb3c705fa41112ef
Advait Junnarkar <advait.ju@gmail.com>
2023-11-08 22:18:07 +1100

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,52 @@
 import './App.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as allIcons from '@fortawesome/free-solid-svg-icons';
+import * as solid from '@fortawesome/free-solid-svg-icons';
+import * as regular from '@fortawesome/free-regular-svg-icons';
+import * as brands from '@fortawesome/free-brands-svg-icons';
 import { useMemo, useState } from 'react';
+
+const icons = [].concat(
+  Object.values(solid),
+  Object.values(regular),
+  Object.values(brands)
+);
 
 function App() {
   const [search, setSearch] = useState('');
-  const [icons, setIcons] = useState(Object.values(allIcons));
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const iconsElements = useMemo(() => {
-    if (search.length < 2) return;
+    if (search.length > 0 && search.length < 2) return;
+    const shownIcons = [];
 
-    return icons.map((icon, index) => {
-      const found = icon?.iconName?.includes(search);
-      if (!found) return;
+    icons.forEach((icon, index) => {
+      if (search.length > 0 && !icon?.iconName?.includes(search)) return;
 
-      return (
+      shownIcons.push(
         <div key={index}>
           <FontAwesomeIcon icon={icon} />
         </div>
       );
     });
+
+    return shownIcons;
   }, [search]);
 
   return (
-    <div>
-      <p>Search</p>
-      <input
-        value={search}
-        onChange={(e) =>
-          setSearch(e.target.value.toLowerCase().replace(/ /g, '-'))
-        }
-        placeholder="Example: arrow, calendar, envelope"
-      />
+    <>
+      <div className="search">
+        <p>Search</p>
+        <input
+          value={search}
+          onChange={(e) =>
+            setSearch(e.target.value.toLowerCase().replace(/ /g, '-'))
+          }
+          placeholder="arrow up, calendar, envelope, twitter"
+        />
+      </div>
       <div className="icons">{iconsElements}</div>
-    </div>
+    </>
   );
 }
 
